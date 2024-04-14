2630b659da81140945bda40bc2f06e863a972b08
ilan schemoul <ilanschemoul@gmail.com>
2024-04-14 17:05:19 +0200

    Sourcing to relative path

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,9 +2,9 @@
 
 require("config/packages")
 
-vim.cmd("source ~/.config/nvim/lua/config/basic.vim")
-vim.cmd("source ~/.config/nvim/lua/config/preferences.vim")
-vim.cmd("source ~/.config/nvim/lua/config/mappings.vim")
+vim.cmd("source ./nvim/lua/config/basic.vim")
+vim.cmd("source ./nvim/lua/config/preferences.vim")
+vim.cmd("source ./nvim/lua/config/mappings.vim")
 
 require("config/clipboard")
 require("config/packages-preferences")
