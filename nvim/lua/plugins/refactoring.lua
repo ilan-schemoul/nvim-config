7f6b8cafd05a30749ccada7ebcedf3ac3d97a715
ilan schemoul <ilanschemoul@gmail.com>
2024-04-24 23:14:59 +0200

    Add gx (fix gx shortcut and improve it too), rm vim-browser-search (replaced by gx which is way better), fix ,G mapping, add refactoring plugin (buggy but useful)

---
+++ nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,60 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  requires = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  enabled = true,
+  lazy = false,
+  keys = {
+    -- extract functions
+    { "<leader>ref", "<cmd>Refactor extract <cr>", mode = "x" },
+    { "<leader>reF", "<cmd>Refactor extract_to_file <cr>", mode = "x" },
+
+    { "<leader>rev", "<cmd>Refactor extract_var <cr>", mode = { "n", "x" } },
+    { "<leader>riv", "<cmd>Refactor inline_var<cr>", mode = { "n", "x" } },
+
+    { "<leader>rif", "<cmd>Refactor inline_func<cr>", mode = "n" },
+
+    { "<leader>reb", "<cmd>Refactor extract_block<cr>", mode = "n" },
+    { "<leader>reF", "<cmd>Refactor extract_block_to_file<cr>", mode = "n" },
+    {
+      mode = "n",
+      "<leader>rpl",
+      function()
+        require("refactoring").debug.printf({ below = true })
+      end,
+    },
+
+    {
+      mode = { "x", "n" },
+      "<leader>rpv",
+      function()
+        require("refactoring").debug.print_var()
+      end,
+    },
+
+    {
+      mode = "n",
+      "<leader>rpc",
+      function()
+        require("refactoring").debug.cleanup({})
+      end,
+    },
+  },
+  opts = {
+    prompt_func_return_type = {
+      go = true,
+      cpp = true,
+      c = true,
+      java = true,
+    },
+    -- prompt for function parameters
+    prompt_func_param_type = {
+      go = true,
+      cpp = true,
+      c = true,
+      java = true,
+    },
+  },
+}
