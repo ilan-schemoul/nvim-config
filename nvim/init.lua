f40f58260e26aadf227015d9aa7d5641b8df0ab5
ilan schemoul <ilanschemoul@gmail.com>
2024-04-26 21:51:47 +0200

    refactoring: remove basic.vim (some part was useless, the rest is moved in preferences.vim), remove debuggers-configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,12 +2,10 @@
 
 require("config/packages")
 
-vim.cmd("source ~/.config/nvim/lua/config/basic.vim")
 vim.cmd("source ~/.config/nvim/lua/config/preferences.vim")
 vim.cmd("source ~/.config/nvim/lua/config/mappings.vim")
 
 require("config/clipboard")
 require("config/packages-preferences")
-require("config/debuggers-configuration")
 
 vim.cmd("source ~/.config/nvim/lua/config/private.vim")
