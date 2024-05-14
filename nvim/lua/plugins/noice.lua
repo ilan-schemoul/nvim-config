7b12adc495af568cfae67ca29013a427e7e27574
ilan schemoul <ilanschemoul@gmail.com>
2024-05-15 00:06:55 +0200

    Fix: noice not ignoring messages such as "10 less lines" when undoing

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -55,75 +55,8 @@ return {
       view = "mini",
       filter = { event = "msg_show" },
     },
-    presets = { inc_rename = true },
 
-    routes = {
-      {
-        event = "notify",
-        kind = "",
-        filter = {
-          any = {
-            { find = "E486" },
-            { find = "search hit" },
-          },
-        },
-        view = "mini",
-      },
-      {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          any = {
-            { find = "no lines in buffer" },
-            -- Edit
-            { find = "%d+ less lines" },
-            { find = "%d+ fewer lines" },
-            { find = "%d+ more lines" },
-            { find = "%d+ change;" },
-            { find = "%d+ line less;" },
-            { find = "%d+ more lines?;" },
-            { find = "%d+ fewer lines;?" },
-            { find = '".+" %d+L, %d+B' },
-            { find = "%d+ lines yanked" },
-            { find = "^Hunk %d+ of %d+$" },
-            { find = "%d+L, %d+B$" },
-            { find = "%d+ changes?;" }, -- Undoing/redoing
-            { find = "%d+ fewer lines" }, -- Deleting multiple lines
-            { find = "%d+ more lines" }, -- Undoing deletion of multiple lines
-            { find = "%d+ lines " }, -- Performing some other verb on multiple lines
-            { find = "Already at newest change" }, -- Redoing
-            { find = '"[^"]+" %d+L, %d+B' }, -- Saving
-
-            -- Save
-            { find = " bytes written" },
-
-            -- Redo/Undo
-            { find = " changes; before #" },
-            { find = " changes; after #" },
-            { find = "1 change; before #" },
-            { find = "1 change; after #" },
-
-            -- Yank
-            { find = " lines yanked" },
-
-            -- Move lines
-            { find = " lines moved" },
-            { find = " lines indented" },
-
-            -- Bulk edit
-            { find = " fewer lines" },
-            { find = " more lines" },
-            { find = "1 more line" },
-            { find = "1 line less" },
-
-            -- General messages
-            { find = "Already at newest change" }, -- Redoing
-            { find = "Already at oldest change" },
-          },
-        },
-        opts = { skip = true },
-      },
-    },
+    routes = require("utils/noice-routes"),
   },
   config = function(_, opts)
     vim.cmd([[
---
+++ nvim/lua/utils/noice-routes.lua
@@ -0,0 +1,31 @@
+return {
+  { view = "split", filter = { event = "msg_show", min_height = 3 } },
+  { filter = { find = "No information available" }, opts = { stop = true } },
+  { filter = { find = "fewer lines;" }, opts = { skip = true } },
+  { filter = { find = "more line;" }, opts = { skip = true } },
+  { filter = { find = "more lines;" }, opts = { skip = true } },
+  { filter = { find = "less;" }, opts = { skip = true } },
+  { filter = { find = "Already at newest" }, view = "mini" },
+  { filter = { find = "Already at oldest" }, view = "mini" },
+  { filter = { find = "change;" }, opts = { skip = true } },
+  { filter = { find = "changes;" }, opts = { skip = true } },
+  { filter = { find = "indent" }, opts = { skip = true } },
+  { filter = { find = "move" }, opts = { skip = true } },
+
+  -- Disable "file_path" AMOUNT_OF_LINESL, AMOUNT_OF_BYTESB message
+  { filter = { event = "msg_show", kind = "", find = '"[%w%p]+" %d+L, %d+B' }, opts = { skip = true } },
+
+  -- Disable "search messages"
+  { filter = { event = "msg_show", kind = "wmsg", find = "search hit BOTTOM, continuing at TOP", }, opts = { skip = true }, },
+  { filter = { event = "msg_show", kind = "wmsg", find = "search hit TOP, continuing at BOTTOM" }, opts = { skip = true }, },
+
+  -- Disable "redo/undo" messages
+  { filter = { event = "msg_show", kind = "lua_error", find = "more line" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "lua_error", find = "fewer line" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "lua_error", find = "line less" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "lua_error", find = "change;" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "", find = "more line" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "", find = "fewer line" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "", find = "line less" }, opts = { skip = true } },
+  { filter = { event = "msg_show", kind = "", find = "change;" }, opts = { skip = true } },
+}
