46cb394b29d952efc1459fd634eab819c32e241d
ilan schemoul <ilanschemoul@gmail.com>
2024-05-17 00:11:01 +0200

    Feat: add plugin command

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,13 @@
--- ~/.config/nvim/lua/config/
-
 require("config/lazy")
 
-vim.cmd("source ~/.config/nvim/lua/config/preferences.vim")
 vim.cmd("source ~/.config/nvim/lua/config/mappings.vim")
+vim.cmd("source ~/.config/nvim/lua/config/preferences.vim")
 
 require("config/clipboard")
+require("config/custom-commands")
 
 if os.getenv("IS_INTERSEC") == "true" then
   vim.cmd("source ~/.config/nvim/lua/config/intersec.vim")
 end
+
+
