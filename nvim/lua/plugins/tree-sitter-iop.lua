392a8bd390f79b43e0fc7b582991a7b394dda9d4
ilan schemoul <ilanschemoul@gmail.com>
2024-05-19 19:02:06 +0200

    Feat: tree-sitter-iop use setup()

--- nvim/lua/plugins/tree-sitter-iop.lua
+++ nvim/lua/plugins/tree-sitter-iop.lua
@@ -1,3 +1,8 @@
 return {
   "https://github.com/ilan-schemoul/tree-sitter-iop",
+  --dir = "~/code/tree-sitter-iop",
+  opts = {
+    -- local_directory = "~/code/tree-sitter-iop/",
+  },
+  build = ":TSInstall iop",
 }
