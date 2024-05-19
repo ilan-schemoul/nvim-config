46ab3cb2f9f789cc423281a5cd4ced3553272f51
ilan schemoul <ilanschemoul@gmail.com>
2024-05-19 19:06:08 +0200

    Feat: update packages

--- nvim/lua/plugins/tree-sitter-iop.lua
+++ nvim/lua/plugins/tree-sitter-iop.lua
@@ -1,6 +1,6 @@
 return {
   "https://github.com/ilan-schemoul/tree-sitter-iop",
-  --dir = "~/code/tree-sitter-iop",
+  -- dir = "~/code/tree-sitter-iop",
   opts = {
     -- local_directory = "~/code/tree-sitter-iop/",
   },
