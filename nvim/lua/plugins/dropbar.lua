e43b8f2a6eb757b013c0c80e9255cdf84bf4c904
ilan schemoul <ilanschemoul@gmail.com>
2024-05-20 23:38:21 +0200

    Feat: enable dropbar, autoreload intersec logs

--- nvim/lua/plugins/dropbar.lua
+++ nvim/lua/plugins/dropbar.lua
@@ -4,8 +4,6 @@ return {
   dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
   },
-  -- Waiting on 0.10 to enable dropbar
-  enabled = false,
   lazy = false,
   keys = {
     { "<leader>op", "<cmd>lua require('dropbar.api').pick()<cr>" },
