035ec5189a127cb044b65aa84b6a16b667392bb5
ilan schemoul <ilanschemoul@gmail.com>
2024-05-26 22:58:10 +0200

    Feat: update package

--- nvim/lua/plugins/logs-context.lua
+++ nvim/lua/plugins/logs-context.lua
@@ -1,6 +1,7 @@
 return {
     "git@github.com:ilan-schemoul/logs-context.git",
     opts = {},
+    enabled = os.getenv("IS_INTERSEC") == "true",
     config = function()
         require("logs-context").setup()
     end,
