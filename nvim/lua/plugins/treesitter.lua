be86240e478097b6971c964f7afae0b7382f59c3
ilan schemoul <ilanschemoul@gmail.com>
2024-06-14 23:57:02 +0200

    Feat: plugin improved ft so f/t (search character in a line) works across lines

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -88,15 +88,6 @@ return {
     },
 
     config = function(_, opts)
-      local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-
-      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-
       if type(opts.ensure_installed) == "table" then
           ---@type table<string, boolean>
           local added = {}
