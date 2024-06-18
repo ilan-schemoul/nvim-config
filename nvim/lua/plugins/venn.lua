b37aa718e1b01bc0579fca6de226c96156bbc223
ilan schemoul <ilanschemoul@gmail.com>
2024-06-18 21:40:15 +0200

    Feat: venn to draw diagram

---
+++ nvim/lua/plugins/venn.lua
@@ -0,0 +1,33 @@
+return {
+  "jbyuki/venn.nvim",
+  config = function()
+    function _G.Toggle_venn()
+      local venn_enabled = vim.inspect(vim.b.venn_enabled)
+
+      if venn_enabled == "nil" then
+        vim.notify("Enabled")
+
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+      else
+        vim.notify("Disabled")
+
+        vim.cmd[[setlocal ve=]]
+        vim.api.nvim_buf_del_keymap(0, "n", "J")
+        vim.api.nvim_buf_del_keymap(0, "n", "K")
+        vim.api.nvim_buf_del_keymap(0, "n", "L")
+        vim.api.nvim_buf_del_keymap(0, "n", "H")
+        vim.api.nvim_buf_del_keymap(0, "v", "f")
+        vim.b.venn_enabled = nil
+      end
+    end
+  end
+}
+
