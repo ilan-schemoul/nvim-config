95d099724b815e5c538f4b2446441dd8d22e8231
Ilan Schemoul <ilan.schemoul@intersec.com>
2024-06-19 10:01:32 +0200

    Fix: conflicting <C-x> with gdb by replacing with <C-q>

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -166,7 +166,7 @@ if os.getenv("KEYBOARD_FR") then
   end
 end
 
-vim.keymap.set("t", "<C-x>", "<c-\\><c-n><cmd>set scrollback=1 | sleep 100m | set scrollback=10000<cr>")
+vim.keymap.set("t", "<C-q>", "<c-\\><c-n><cmd>set scrollback=1 | sleep 100m | set scrollback=10000<cr>")
 
 vim.cmd([[
   " Custom env variable
