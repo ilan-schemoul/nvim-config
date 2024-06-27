0e1bdb30283c94c3e75631afa9a4cd899b765882
ilan schemoul <ilanschemoul@gmail.com>
2024-06-27 23:15:44 +0200

    Feat: enable venn always in norg

--- nvim/lua/config/custom-commands.lua
+++ nvim/lua/config/custom-commands.lua
@@ -212,3 +212,46 @@ function _G.OpenUnusedTermOrCreate()
 
   vim.cmd(":term")
 end
+
+local function enable_venn()
+  vim.notify("Enabled")
+
+  vim.b.venn_enabled = true
+  vim.cmd[[setlocal ve=all]]
+  vim.cmd[[setlocal paste]]
+  -- draw a line on HJKL keystokes
+  vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+  vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+  -- draw a box by pressing "f" with visual selection
+  vim.api.nvim_buf_set_keymap(0, "v", "b", ":VBox<CR>", {noremap = true})
+end
+
+function _G.Toggle_venn()
+  local venn_enabled = vim.inspect(vim.b.venn_enabled)
+
+  if venn_enabled == "nil" then
+    enable_venn()
+  else
+    vim.notify("Disabled")
+
+    vim.cmd[[setlocal ve=]]
+    vim.cmd[[setlocal nopaste]]
+    vim.api.nvim_buf_del_keymap(0, "n", "J")
+    vim.api.nvim_buf_del_keymap(0, "n", "K")
+    vim.api.nvim_buf_del_keymap(0, "n", "L")
+    vim.api.nvim_buf_del_keymap(0, "n", "H")
+    vim.api.nvim_buf_del_keymap(0, "v", "b")
+    vim.b.venn_enabled = nil
+  end
+end
+
+vim.api.nvim_create_autocmd({
+    "FileType",
+  },
+  {
+    pattern = "norg",
+    callback = enable_venn,
+})
+
