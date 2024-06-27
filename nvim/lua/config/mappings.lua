afdf6a7c4392de1fe8b8afb041bfd77934d27787
ilan schemoul <ilanschemoul@gmail.com>
2024-06-27 22:27:09 +0200

    Refactor: simplify mapping helper function

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -4,72 +4,64 @@ local get_tab_folder = require("config/utils").get_tab_folder
 
 local fr = { "à", "&", "é", "\"", "'", "(", "-", "è", "_", "ç" }
 
-local function add_keymap(modes, keys, cmd)
-  for _, mode in ipairs(modes) do
-    if mode == "t" then
-      vim.keymap.set(mode, "," .. keys, cmd, { silent = true })
-    elseif mode == "i" then
-      vim.keymap.set(mode, keys, cmd, { silent = true })
-    else
-      vim.keymap.set(mode, "<leader>" .. keys, cmd, { silent = true })
-    end
-  end
+local function set(keys, cmd)
+  vim.keymap.set("n", "<leader>" .. keys, cmd)
 end
 
 -- Many mappings defined inside plugins (grep "keys =" to find those)
 
-add_keymap({ "n" }, "[d", vim.diagnostic.goto_prev)
-add_keymap({ "n" }, "]d", vim.diagnostic.goto_next)
+set("[d", vim.diagnostic.goto_prev)
+set("]d", vim.diagnostic.goto_next)
 
-add_keymap({ "n" }, "lD", vim.diagnostic.open_float)
-add_keymap({ "n" }, "lh", vim.lsp.buf.hover)
-add_keymap({ "n" }, "li", "<cmd>Telescope lsp_references<cr>")
-add_keymap({ "n" }, "ld", "<cmd>Telescope lsp_definitions<cr>")
-add_keymap({ "n" }, "lb", "<cmd>Telescope diagnostic<cr>")
-add_keymap({ "n" }, "ls", "<cmd>Telescope lsp_workspace_symbols<cr>")
-add_keymap({ "n" }, "lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>")
-add_keymap({ "n" }, "ln", vim.lsp.buf.rename)
-add_keymap({ "n" }, "la", vim.lsp.buf.code_action)
-add_keymap({ "n" }, "lt", require("lsp_lines").toggle)
-add_keymap({ "n" }, "ll", "<cmd>Telescope smart_open<cr>")
+set("lD", vim.diagnostic.open_float)
+set("lh", vim.lsp.buf.hover)
+set("li", "<cmd>Telescope lsp_references<cr>")
+set("ld", "<cmd>Telescope lsp_definitions<cr>")
+set("lb", "<cmd>Telescope diagnostic<cr>")
+set("ls", "<cmd>Telescope lsp_workspace_symbols<cr>")
+set("lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>")
+set("ln", vim.lsp.buf.rename)
+set("la", vim.lsp.buf.code_action)
+set("lt", require("lsp_lines").toggle)
+set("ll", "<cmd>Telescope smart_open<cr>")
 
-add_keymap({ "n" }, "o", _G.OpenFile)
-add_keymap({ "n" }, ";", _G.OpenFileWithExtension)
-add_keymap({ "n" }, ".", _G.OpenFileWithExtension)
+set("o", _G.OpenFile)
+set(";", _G.OpenFileWithExtension)
+set(".", _G.OpenFileWithExtension)
 
-add_keymap({ "i" }, "<C-s>", vim.lsp.buf.signature_help)
+vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 
-add_keymap({ "n" }, "A", "<cmd>NodeAction<cr>")
+set("A", "<cmd>NodeAction<cr>")
 
-add_keymap({ "n" }, "bn", "<cmd>FocusSplitNicely<cr>")
-add_keymap({ "n" }, "bg", function()
+set("bn", "<cmd>FocusSplitNicely<cr>")
+set("bg", function()
   vim.cmd("FocusSplitNicely")
   vim.cmd("Telescope live_grep")
 end)
-add_keymap({ "n" }, "bl", function()
+set("bl", function()
   vim.cmd("FocusSplitNicely")
   vim.cmd("Telescope smart_open")
 end)
-add_keymap({ "n" }, "bx", _G.CloseBuffer)
+set("bx", _G.CloseBuffer)
 
-add_keymap({ "n" }, "N", "<cmd>set invrelativenumber | set invnumber<cr>")
+set("N", "<cmd>set invrelativenumber | set invnumber<cr>")
 
-add_keymap({ "n" }, "q", _G.CloseWindowIfNotLast)
-add_keymap({ "n" }, "Q", "<cmd>qa!<cr>")
+set("q", _G.CloseWindowIfNotLast)
+set("Q", "<cmd>qa!<cr>")
 
-add_keymap({ "n" }, "m", "<cmd>Mason<cr>")
+set("m", "<cmd>Mason<cr>")
 
-add_keymap({ "n" }, "F", "<cmd>echo @%<cr>")
+set("F", "<cmd>echo @%<cr>")
 
-add_keymap({ "n" }, "u", "<cmd>Telescope undo<cr>")
+set("u", "<cmd>Telescope undo<cr>")
 
-add_keymap({ "n" }, "tt", "<cmd>Telescope<cr>")
-add_keymap({ "n" }, "tg", "<cmd>Telescope live_grep<cr>")
-add_keymap({ "n" }, "tr", "<cmd>Telescope resume<cr>")
-add_keymap({ "n" }, "tz", "<cmd>Telescope buffers<cr>")
-add_keymap({ "n" }, "tq", "<cmd>Telescope quickfix<cr>")
+set("tt", "<cmd>Telescope<cr>")
+set("tg", "<cmd>Telescope live_grep<cr>")
+set("tr", "<cmd>Telescope resume<cr>")
+set("tz", "<cmd>Telescope buffers<cr>")
+set("tq", "<cmd>Telescope quickfix<cr>")
 
-add_keymap({ "n" }, "tw", function()
+set("tw", function()
   require("telescope-tabs").list_tabs({
     entry_formatter = function(tab_id, _, _, _, _)
       return get_tab_folder(tab_id)
@@ -79,77 +71,77 @@ add_keymap({ "n" }, "tw", function()
     end,
   })
 end)
-add_keymap({ "n" }, "t-", require("telescope-tabs").go_to_previous)
+set("t-", require("telescope-tabs").go_to_previous)
 
 for i = 0, 9 do
   if os.getenv("KEYBOARD_FR") then
-    add_keymap({ "n" }, "t" .. fr[i + 1], "<cmd>" .. tostring(i) .. "tabn" .. "<cr>")
+    set("t" .. fr[i + 1], "<cmd>" .. tostring(i) .. "tabn" .. "<cr>")
   end
 
-  add_keymap({ "n" }, "t" .. tostring(i), "<cmd>" .. tostring(i) .. "tabn" .. "<cr>")
+  set("t" .. tostring(i), "<cmd>" .. tostring(i) .. "tabn" .. "<cr>")
 end
 
-add_keymap({ "n" }, "tq", function() require("telescope.builtin").quickfix({
+set("tq", function() require("telescope.builtin").quickfix({
   trim_text = true,
   path_display = { "smart" }
 }) end)
 
-add_keymap({ "n" }, "pn", _G.OpenUnusedTermOrCreate)
-add_keymap({ "n" }, "pN", "<cmd>term<cr>")
-add_keymap({ "n" }, "ph", "<cmd>vsplit | lua _G.OpenUnusedTermOrCreate()<cr>")
-add_keymap({ "n" }, "pj", "<cmd>belowright split | lua _G.OpenUnusedTermOrCreate()<cr>")
-add_keymap({ "n" }, "pk", "<cmd>topleft split | lua _G.OpenUnusedTermOrCreate()<cr>")
-add_keymap({ "n" }, "pl", "<cmd>botright vs | lua _G.OpenUnusedTermOrCreate()<cr>")
-add_keymap({ "n" }, "pH", "<cmd>vsplit | term <cr>")
-add_keymap({ "n" }, "pJ", "<cmd>belowright split | term <cr>")
-add_keymap({ "n" }, "pK", "<cmd>topleft split | term <cr>")
-add_keymap({ "n" }, "pL", "<cmd>botright vs | term <cr>")
-add_keymap({ "n" }, "pr", "<cmd>SendToTerm !!<cr>")
-add_keymap({ "n" }, "ps", "<cmd>SendToTerm<cr>")
+set("pn", _G.OpenUnusedTermOrCreate)
+set("pN", "<cmd>term<cr>")
+set("ph", "<cmd>vsplit | lua _G.OpenUnusedTermOrCreate()<cr>")
+set("pj", "<cmd>belowright split | lua _G.OpenUnusedTermOrCreate()<cr>")
+set("pk", "<cmd>topleft split | lua _G.OpenUnusedTermOrCreate()<cr>")
+set("pl", "<cmd>botright vs | lua _G.OpenUnusedTermOrCreate()<cr>")
+set("pH", "<cmd>vsplit | term <cr>")
+set("pJ", "<cmd>belowright split | term <cr>")
+set("pK", "<cmd>topleft split | term <cr>")
+set("pL", "<cmd>botright vs | term <cr>")
+set("pr", "<cmd>SendToTerm !!<cr>")
+set("ps", "<cmd>SendToTerm<cr>")
 
-add_keymap({ "n" }, "pp", "<cmd>put<cr>")
-add_keymap({ "n" }, "pP", "<cmd>put!<cr>")
+set("pp", "<cmd>put<cr>")
+set("pP", "<cmd>put!<cr>")
 
-add_keymap({ "n" }, "tf", "<cmd>lua require('telescope-tabs').list_tabs()<cr>")
-add_keymap({ "n" }, "tn", "<cmd>tabnew<cr>")
-add_keymap({ "n" }, "tx", "<cmd>tabclose<cr>")
-add_keymap({ "n" }, "tl", "<cmd>tabnext<cr>")
-add_keymap({ "n" }, "th", "<cmd>tabprevious<cr>")
-add_keymap({ "n" }, "tL", "<cmd>+tabmove<cr>")
-add_keymap({ "n" }, "tH", "<cmd>-tabmove<cr>")
+set("tf", "<cmd>lua require('telescope-tabs').list_tabs()<cr>")
+set("tn", "<cmd>tabnew<cr>")
+set("tx", "<cmd>tabclose<cr>")
+set("tl", "<cmd>tabnext<cr>")
+set("th", "<cmd>tabprevious<cr>")
+set("tL", "<cmd>+tabmove<cr>")
+set("tH", "<cmd>-tabmove<cr>")
 
-add_keymap({ "n" }, "vc", "<cmd>next ~/.config/nvim/init.lua<cr>")
-add_keymap({ "n" }, "vp", "<cmd>next ~/.config/nvim/lua/plugins<cr>")
-add_keymap({ "n" }, "vm", "<cmd>next ~/.config/nvim/lua/config/mappings.lua<cr>")
-add_keymap({ "n" }, "vg", "<cmd>Telescope live_grep search_dirs=~/.config/nvim<cr>")
-add_keymap({ "n" }, "vl", "<cmd>Telescope find_files search_dirs=~/.config/nvim<cr>")
-add_keymap({ "n" }, "vt", "<cmd>e ~/nvim-main/todo.norg<cr>")
-add_keymap({ "n" }, "vv", "<cmd>mapclear | source ~/.config/nvim/init.lua<cr>")
+set("vc", "<cmd>next ~/.config/nvim/init.lua<cr>")
+set("vp", "<cmd>next ~/.config/nvim/lua/plugins<cr>")
+set("vm", "<cmd>next ~/.config/nvim/lua/config/mappings.lua<cr>")
+set("vg", "<cmd>Telescope live_grep search_dirs=~/.config/nvim<cr>")
+set("vl", "<cmd>Telescope find_files search_dirs=~/.config/nvim<cr>")
+set("vt", "<cmd>e ~/nvim-main/todo.norg<cr>")
+set("vv", "<cmd>mapclear | source ~/.config/nvim/init.lua<cr>")
 
-add_keymap({ "n" }, "nm", "<cmd>e ~/notes/memory.norg<cr>")
-add_keymap({ "n" }, "nM", "<cmd>botright 30vnew ~/notes/memory.norg | set invrelativenumber | set invnumber<cr>")
-add_keymap({ "n" }, "nl", "<cmd>Telescope find_files search_dirs={'~/notes'} follow=true<cr>")
-add_keymap({ "n" }, "ng", "<cmd>Telescope live_grep search_dirs={'~/notes'}<cr>")
-add_keymap({ "n" }, "nn", _G.create_org_file)
+set("nm", "<cmd>e ~/notes/memory.norg<cr>")
+set("nM", "<cmd>botright 30vnew ~/notes/memory.norg | set invrelativenumber | set invnumber<cr>")
+set("nl", "<cmd>Telescope find_files search_dirs={'~/notes'} follow=true<cr>")
+set("ng", "<cmd>Telescope live_grep search_dirs={'~/notes'}<cr>")
+set("nn", _G.create_org_file)
 -- ../plugins/venn.lua
-add_keymap({ "n" }, "nd", ":lua Toggle_venn()<cr>")
+set("nd", ":lua Toggle_venn()<cr>")
 
 -- ../plugins/treesitter.lua
-add_keymap({ "n" }, "sc", "1z=")
+set("sc", "1z=")
 -- Can't use <leader>sl as it is used by for TS swapping
-add_keymap({ "n" }, "s=", "<cmd>CustomTelescopeSpellSuggest<cr>")
-add_keymap({ "n" }, "sr", "<cmd>spellr<cr>")
-add_keymap({ "n" }, "sg", "zg")
-add_keymap({ "n" }, "sw", "zw")
-add_keymap({ "n" }, "sb", "zw")
+set("s=", "<cmd>CustomTelescopeSpellSuggest<cr>")
+set("sr", "<cmd>spellr<cr>")
+set("sg", "zg")
+set("sw", "zw")
+set("sb", "zw")
 
 -- See also ../plugins/gitsigns.lua
-add_keymap({ "n" }, "gl", "<cmd>G log -50<cr>")
-add_keymap({ "n" }, "gp", "<cmd>G push<cr>")
-add_keymap({ "n" }, "gg", "<cmd>G pull<cr>")
+set("gl", "<cmd>G log -50<cr>")
+set("gp", "<cmd>G push<cr>")
+set("gg", "<cmd>G pull<cr>")
 
 -- We also disable jk (houdini) for lazygit
-add_keymap({ "n" }, "go", function()
+set("go", function()
   local path = vim.fn.expand('%:h')
 
   if path:find("term://") or not vim.fn.filereadable(path) then
@@ -180,7 +172,7 @@ vim.api.nvim_create_autocmd({
     end
   })
 
-add_keymap({ "n" }, "gq", require("config/gerrit-quickfix").load_interactive_input)
+set("gq", require("config/gerrit-quickfix").load_interactive_input)
 
 for _, key in ipairs({ "h", "j", "k", "l" }) do
   vim.keymap.set({ "t", "n", "i" }, "<A-" .. key .. ">", "<C-\\><C-N><C-w>" .. key)
