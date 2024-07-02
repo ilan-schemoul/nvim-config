f270eefb1f990414bfd91f398b8c5ce5092df568
Ilan Schemoul <ilan.schemoul@intersec.com>
2024-07-02 10:47:49 +0200

    Fix: cw

--- nvim/lua/plugins/nvim-spider.lua
+++ nvim/lua/plugins/nvim-spider.lua
@@ -4,9 +4,13 @@ return {
     for _, key in ipairs({ "b", "w", "e" }) do
       vim.keymap.set({ "n", "o", "x" }, key, "<cmd>lua require('spider').motion('" .. key .. "')<CR>", {})
     end
+
     require("spider").setup({
       subwordMovement = false,
     })
+
+    -- So cw stays the same as before
+    vim.keymap.set("n", "cw", "ce", { remap = true })
   end,
 }
 
