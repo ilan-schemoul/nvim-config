f9e578af119c96d3bd93e219a040b482be3c4f13
ilan schemoul <ilanschemoul@gmail.com>
2024-07-04 23:10:19 +0200

    fix: performance lazy-load telescope

--- nvim/lua/plugins/smart-open.lua
+++ nvim/lua/plugins/smart-open.lua
@@ -1,14 +1,18 @@
 return {
   "danielfalk/smart-open.nvim",
-  event = "VeryLazy",
   dependencies = {
     "kkharji/sqlite.lua",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
-  init = function()
-    require("telescope").load_extension("smart_open")
-  end,
+  lazy = true,
+  keys = {
+    { "<leader>ll", "<cmd>Telescope smart_open<cr>" },
+  },
   opts = {
     match_algorithm = "fzf",
   },
+  config = function(_, opts)
+    require("smart-open").setup(opts)
+    require("telescope").load_extension("smart_open")
+  end,
 }
