ec4807dbd96606d97906823b32b1d21a73678af8
ilan schemoul <ilanschemoul@gmail.com>
2024-07-05 22:49:51 +0200

    refactor

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,19 +4,6 @@ return {
   priority = 1000,
   lazy = true,
   opts = {
-    integrations = {
-      gitsigns = true,
-      nvimtree = true,
-      treesitter = true,
-      notify = true,
-      mason = true,
-      neotest = true,
-      dap = true,
-      dap_ui = true,
-      rainbow_delimiters = true,
-      telescope = { enabled = true },
-      illuminate = { enabled = true, lsp = false },
-      dashboard = true,
-    },
+    default_integrations = true,
   },
 }
