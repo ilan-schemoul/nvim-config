9f60b14eece2f9fdc51b8c71627a5394baa4a3c6
ilan schemoul <ilanschemoul@gmail.com>
2024-07-05 22:06:17 +0200

    feat: mapping for fuzzy finding inside file and new keymaps for grep
    Keymaps for both fuzzy finding and grep in 3 different modes:
    - If mode is selection => use current selection
    - If uppercase (e.g: <leader>tG) => use word under cursor
    - Else => open telescope without anything as always

--- nvim/lua/config/custom-commands.lua
+++ nvim/lua/config/custom-commands.lua
@@ -330,3 +330,15 @@ vim.api.nvim_create_user_command("FromFTToTab", function(args)
   end
 end, { nargs = '+' })
 
+function _G.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
