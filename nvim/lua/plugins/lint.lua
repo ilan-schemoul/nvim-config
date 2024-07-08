b962e79eedb9997afb049b33d85fa4e7f0c365e7
Ilan Schemoul <ilan.schemoul@intersec.com>
2024-07-08 12:13:56 +0200

    Feat: use pylint manually because conflict with preinstalled pylint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -4,12 +4,17 @@ return {
   dependencies = {
     "frostplexx/mason-bridge.nvim",
   },
+  opts = {},
   config = function()
     local timer = vim.uv.new_timer()
     -- I don't now why but it works
     timer:start(0, 0, vim.schedule_wrap(function()
       require("lint").linters_by_ft = require("mason-bridge").get_linters()
+      require("lint").linters_by_ft.python = { "pylint" }
     end))
+    -- require("lint").linters_by_ft = {
+    --   python = { "pylint" },
+    -- }
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
