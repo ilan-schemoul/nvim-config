668ac719086fe5cea8d6101911c68103c82883d0
ilan schemoul <ilanschemoul@gmail.com>
2024-07-13 18:47:07 +0200

    feat: use new version blame which fix open commit

--- nvim/lua/plugins/blame.lua
+++ nvim/lua/plugins/blame.lua
@@ -1,7 +1,10 @@
 return {
   -- "FabijanZulj/blame.nvim",
   -- https://github.com/FabijanZulj/blame.nvim/pull/46
+  -- https://github.com/FabijanZulj/blame.nvim/pull/48
   "ilan-schemoul/blame.nvim",
+  -- dir = "~/code/forks/blame.nvim/",
+  branch = "master",
   keys = {
     { "gb", ":BlameToggle<cr>" },
   },
