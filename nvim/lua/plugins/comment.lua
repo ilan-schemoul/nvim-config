74890a0854a90f0b72b9e0d7b90cef60649e6430
ilan schemoul <ilanschemoul@gmail.com>
2024-07-21 11:10:13 +0200

    feat: block comments only for c files

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,11 +1,12 @@
 -- TODO: With 0.10 comes native comment support
 return {
     "ilan-schemoul/Comment.nvim",
+    -- dir = "~/code/forks/Comment.nvim",
     event = "VimEnter",
     opts = {
         padding = true,
         sticky = true,
-        always_use_block = true,
+        only_block_ft = { 'c' },
         toggler = {
             line = "<leader>cc",
             block = "<leader>CC",
