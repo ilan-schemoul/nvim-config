2c16005ca368b31be475963fb53ea6c40490a134
ilan schemoul <ilanschemoul@gmail.com>
2024-07-27 23:49:39 +0200

    fix: fork true-zen to fix crash when leaving zen mode and fix the floatings 1s

--- nvim/lua/plugins/true-zen.lua
+++ nvim/lua/plugins/true-zen.lua
@@ -1,14 +1,30 @@
 return {
-  "pocco81/true-zen.nvim",
+  -- "pocco81/true-zen.nvim",
+  -- dir = "~/code/forks/true-zen.nvim",
+  -- https://github.com/pocco81/true-zen.nvim/pull/134
+  "ilan-schemoul/true-zen.nvim",
+  keys = {
+    { "<leader>z", "<cmd>TZAtaraxis<cr>" }
+  },
   opts = {
     modes = { -- configurations per mode
       ataraxis = {
+        shade = "dark", -- if `dark` then dim the padding windows, otherwise if it's `light` it'll brighten said windows
+        padding = { -- padding windows
+          left = 38,
+          right = 38,
+        },
+        quit_untoggles = true, -- type :q or :qa to quit Ataraxis mode
         callbacks = { -- run functions when opening/closing Ataraxis mode
-          open_post = function()
+          open_pos = function()
             vim.cmd("ScrollbarHide")
+            vim.cmd("setl nonumber")
+            vim.cmd("setl norelativenumber")
           end,
           close_pos = function()
             vim.cmd("ScrollbarShow")
+            vim.cmd("setl number")
+            vim.cmd("setl relativenumber")
           end,
         },
       }
