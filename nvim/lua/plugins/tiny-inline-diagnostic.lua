57963c864d08067caecdafd4231221e249083d9b
ilan schemoul <ilanschemoul@gmail.com>
2024-07-27 23:48:23 +0200

    feat: enable tiny-inline-diagnostic as it now supports multiline diagnostics

--- nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -1,11 +1,13 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
-  -- https://github.com/rachartier/tiny-inline-diagnostic.nvim/issues/11
-  enabled = false,
   event = "VeryLazy",
   init = function()
     vim.diagnostic.config({ virtual_text = false })
   end,
-  opts = {},
+  opts = {
+    options = {
+        multilines = true,
+    },
+  },
 }
 
