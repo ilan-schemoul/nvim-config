381cd1d14db3741cd5abb5f89447d0f2a2b385b3
Ilan Schemoul <ilan.schemoul@intersec.com>
2024-10-14 10:09:59 +0200

    feat: add shortcut to open list of files changed in previous commit

--- nvim/lua/plugins/easypick.lua
+++ nvim/lua/plugins/easypick.lua
@@ -6,6 +6,28 @@ return {
   },
   config = function()
     local easypick = require("easypick")
+    local previewers = require "telescope.previewers"
+    local putils = require "telescope.previewers.utils"
+    local from_entry = require "telescope.from_entry"
+    local conf = require("telescope.config").values
+
+    local diff_previous_commit = function (opts)
+      return previewers.new_buffer_previewer {
+        title = "Git Previous Commit Diff Preview",
+        get_buffer_by_name = function(_, entry)
+          return entry.value
+        end,
+
+        define_preview = function(self, entry, _)
+          local file_name = entry.value
+          putils.job_maker({ "git", "--no-pager", "diff", "HEAD~", "--", file_name }, self.state.bufnr, {
+            value = file_name,
+            bufname = self.state.bufname,
+          })
+          putils.regex_highlighter(self.state.bufnr, "diff")
+        end,
+      }
+    end
 
     easypick.setup({
       pickers = {
@@ -14,6 +36,12 @@ return {
           name = "changed_files",
           command = "git diff --name-only",
           previewer = easypick.previewers.file_diff()
+        },
+
+        {
+          name = "changed_files_previous_commit",
+          command = "git diff --name-only HEAD~",
+          previewer = diff_previous_commit()
         },
 
         -- list files that have conflicts with diffs in preview
