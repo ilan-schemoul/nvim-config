d851342c5e822fdf9314571f2a30a619f5c6bcca
Ilan Schemoul <ilan.schemoul@intersec.com>
2024-10-28 09:54:54 +0100

    refactor: use upstream blame again (as my change was merged)

--- nvim/lua/plugins/blame.lua
+++ nvim/lua/plugins/blame.lua
@@ -1,10 +1,5 @@
 return {
-  -- "FabijanZulj/blame.nvim",
-  -- https://github.com/FabijanZulj/blame.nvim/pull/46
-  -- https://github.com/FabijanZulj/blame.nvim/pull/48
-  "ilan-schemoul/blame.nvim",
-  -- dir = "~/code/forks/blame.nvim/",
-  branch = "master",
+  "FabijanZulj/blame.nvim",
   keys = {
     { "gb", ":BlameToggle<cr>" },
   },
