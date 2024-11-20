133c0567297bb50f4c593c5bee1b1610a021d385
ilan schemoul <ilanschemoul@gmail.com>
2024-11-20 22:30:03 +0100

    refactor: clipboard use native OSC52 and native windows command for WSL
    Instead of plugin use native (from 0.10) osc52 code of nvim
    Instead of external binary use powershell for copy/paste on WSL

--- nvim/lua/config/clipboard.lua
+++ nvim/lua/config/clipboard.lua
@@ -1,28 +1,35 @@
-if os.getenv("WSL_DISTRO_NAME") ~= nil then
+if os.getenv("SSH_TTY") ~= nil then
+  local function paste()
+    return {
+      vim.fn.split(vim.fn.getreg(""), "\n"),
+      vim.fn.getregtype(""),
+    }
+  end
+
   vim.g.clipboard = {
-    name = "win32yank-wsl",
+    name = 'OSC 52',
     copy = {
-      ["+"] = "win32yank.exe -i --crlf",
-      ["*"] = "win32yank.exe -i --crlf",
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    -- For security reasons pasting is usually not supported by terminals for OSC52
+    -- So we don't use clipboard.osc52 for pasting. Otherwise nvim hangs when pasting.
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+elseif os.getenv("WSL_DISTRO_NAME") then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
     },
     paste = {
-      ["+"] = "win32yank.exe -o --lf",
-      ["*"] = "win32yank.exe -o --lf",
+      ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
     },
     cache_enabled = 0,
-  }
-elseif vim.fn.executable("osc") ~= 0 then
-  local function copy(lines, _)
-    require("osc52").copy(table.concat(lines, "\n"))
-  end
-
-  local function paste()
-    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-  end
-
-  vim.g.clipboard = {
-    name = "osc52",
-    copy = { ["+"] = copy, ["*"] = copy },
-    paste = { ["+"] = paste, ["*"] = paste },
   }
 end
