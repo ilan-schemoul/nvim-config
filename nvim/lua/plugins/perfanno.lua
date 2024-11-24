535a2b6dd6be12e88e04e78bc7d5bd307e1679c8
ilan schemoul <ilanschemoul@gmail.com>
2024-11-24 01:45:39 +0100

    fix: perfanno annotate

--- nvim/lua/plugins/perfanno.lua
+++ nvim/lua/plugins/perfanno.lua
@@ -1,5 +1,12 @@
 return {
   "t-troebst/perfanno.nvim",
-  opts = {},
+  config = function()
+    local util = require("perfanno.util")
+
+    require("perfanno").setup({
+      line_highlights = util.make_bg_highlights("#FFFFFF", "#AA0000", 10),
+      vt_highlight = util.make_fg_highlight("#AA0000"),
+    })
+  end
 }
 
