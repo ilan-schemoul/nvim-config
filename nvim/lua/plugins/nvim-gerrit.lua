3e6f09c6944b79e5bef763c6c40382517b39c78a
ilan schemoul <ilanschemoul@gmail.com>
2024-11-30 19:40:03 +0100

    feat: add HTTP method to connect to gerrit

--- nvim/lua/plugins/nvim-gerrit.lua
+++ nvim/lua/plugins/nvim-gerrit.lua
@@ -1,12 +1,18 @@
 local utils = require("config/utils")
+local FORCE_ENABLE_PLUGIN = false
 
 return {
   "ilan-schemoul/nvim-gerrit",
   cmd = { "GerritLoadComments" },
-  enabled = utils.is_intersec(),
+  enabled = utils.is_intersec() or FORCE_ENABLE_PLUGIN,
   opts = {
-    url = "https://git.corp",
-    cookie = os.getenv("GERRIT_COOKIES"),
+    url = "https://git.corp/a",
+    -- cookie = os.getenv("GERRIT_COOKIE"),
+    -- TODO:use something like 1password to be more safe. (gerrit plugin
+    -- must be updated to support functions instead of just strings)
+    digest_authentication = false,
+    username = os.getenv("GERRIT_USERNAME"),
+    password = os.getenv("GERRIT_PASSWORD"),
     debug = true,
   },
 }
