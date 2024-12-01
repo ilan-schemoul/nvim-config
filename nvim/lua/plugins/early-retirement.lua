1c3b219e12f5fe6b25cd9d979531c0d782853caa
ilan schemoul <ilanschemoul@gmail.com>
2024-12-01 23:53:21 +0100

    fix: early retirement bug where it closes entire tab

--- nvim/lua/plugins/early-retirement.lua
+++ nvim/lua/plugins/early-retirement.lua
@@ -1,7 +1,13 @@
+local custom_commands = require("config/custom-commands")
+
 return {
-    "chrisgrieser/nvim-early-retirement",
+    "ilan-schemoul/nvim-early-retirement",
     opts = {
-        -- retirementAgeMins = 60 * 1,
+      -- 1h30
+        retirementAgeMins = 90,
+        -- This is what my fork provides.
+        -- Necessary as to not close any tab.
+        deleteFunction = custom_commands.close_buffer_if_not_last,
     },
     event = "VeryLazy",
 }
