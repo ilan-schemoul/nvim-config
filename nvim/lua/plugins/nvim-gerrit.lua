5b8d4f90fb7ffbc8a89db6e2e9601c7538f5f97e
ilan schemoul <ilanschemoul@gmail.com>
2024-12-01 13:08:18 +0100

    fix: one gerrit config for intersec, one for test

--- nvim/lua/plugins/nvim-gerrit.lua
+++ nvim/lua/plugins/nvim-gerrit.lua
@@ -1,27 +1,40 @@
 local utils = require("config/utils")
-local FORCE_ENABLE_PLUGIN = false
 
 return {
   "ilan-schemoul/nvim-gerrit",
-  cmd = { "GerritLoadComments" },
-  enabled = utils.is_intersec() or FORCE_ENABLE_PLUGIN,
-  opts = {
-    url = "https://git.corp",
-    cookie = function()
-      local handle = io.popen("cp ~/snap/firefox/common/.mozilla/firefox/*default-release/cookies.sqlite /tmp/cookies.sqlite "
-                              .. "&& sqlite3 /tmp/cookies.sqlite 'SELECT value FROM moz_cookies WHERE name=\"GerritAccount\" ;'"
-                              .. "&& rm /tmp/cookies.sqlite")
-      assert(handle, "Gerrit cookie: popen fail")
-      local result = handle:read("*a")
-      assert(handle, "Gerrit cookie: failed to read output")
-      handle:close()
-      assert(#result, "Gerrit cookie: no output")
-      -- Remove the \n
-      result = result:sub(1, -2)
-      return "GerritAccount=" .. result
-    end,
-    debug = false,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
   },
-  setup = function()
+  -- TODO: add dependency
+  cmd = { "GerritLoadComments" },
+  config = function()
+    local qt_opts = {
+      url = "https://codereview.qt-project.org/a",
+      digest_authentication = false,
+      username = os.getenv("GERRIT_USERNAME"),
+      password = os.getenv("GERRIT_PASSWORD"),
+      debug = true,
+    }
+
+    local intersec_opts = {
+      url = "https://git.corp",
+      cookie = function()
+        local handle = io.popen("cp ~/snap/firefox/common/.mozilla/firefox/*default-release/cookies.sqlite /tmp/cookies.sqlite "
+        .. "&& sqlite3 /tmp/cookies.sqlite 'SELECT value FROM moz_cookies WHERE name=\"GerritAccount\" ;'"
+        .. "&& rm /tmp/cookies.sqlite")
+        assert(handle, "Gerrit cookie: popen fail")
+        local result = handle:read("*a")
+        assert(handle, "Gerrit cookie: failed to read output")
+        handle:close()
+        assert(#result, "Gerrit cookie: no output")
+        -- Remove the \n
+        result = result:sub(1, -2)
+        return "GerritAccount=" .. result
+      end,
+      debug = false,
+    }
+
+    local opts = utils.is_intersec() and intersec_opts or qt_opts
+    require("nvim-gerrit").setup(opts)
   end,
 }
