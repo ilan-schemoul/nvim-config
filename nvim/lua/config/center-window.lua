e13ae1e856d36a7b7512a59cac480e34bc982d7d
ilan schemoul <ilanschemoul@gmail.com>
2024-12-01 16:42:44 +0100

    feat: center window with memory buffer on the right and terminal on the left by default

--- nvim/lua/config/center-window.lua
+++ nvim/lua/config/center-window.lua
@@ -1,24 +1,27 @@
-local center = {}
+local function center(left_buffer, right_buffer)
+  left_buffer = left_buffer or "term"
+  right_buffer = right_buffer or "~/notes/memory.norg"
 
-function center.focus()
   local width = vim.fn.winwidth(0)
-  local split_width = width / 2
+  local split_width = width / 4
 
   -- New tab
   vim.cmd("tabedit %")
 
   local window = vim.api.nvim_get_current_win()
+  local right_cmd = right_buffer and "belowright vs " .. right_buffer or "vert belowright new"
 
-  -- Right buffer
-  vim.cmd("vert belowright new | vertical resize " .. split_width)
+  -- Left buffer
+  vim.cmd("vert " .. left_buffer)
+  vim.cmd("vertical resize " .. split_width)
   vim.cmd("set norelativenumber | set nonumber")
   -- Disable color for current line
   vim.cmd("highlight CursorLine guifg=NONE guibg=NONE")
 
   vim.api.nvim_set_current_win(window)
 
-  -- Left buffer
-  vim.cmd("vert new | vertical resize " .. split_width)
+  -- Right buffer
+  vim.cmd(right_cmd .. " | vertical resize " .. split_width)
   vim.cmd("set norelativenumber | set nonumber")
   -- Disable color for current line
   vim.cmd("highlight CursorLine guifg=NONE guibg=NONE")
@@ -26,9 +29,13 @@ function center.focus()
   vim.api.nvim_set_current_win(window)
 end
 
-function center.close()
+local function close()
   vim.cmd("tabclose")
   vim.cmd("tabprevious")
 end
 
-return center
+return {
+  center = center,
+  center_with_memory = center_with_memory,
+  close = close,
+}
