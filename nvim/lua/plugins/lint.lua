bed9a8ae7f8f4aaccb40ac24822a0b1ba5b16595
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-01-22 15:44:58 +0100

    fix: pylint in intersec

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,24 +1,21 @@
 return {
   "mfussenegger/nvim-lint",
-  priority = 0,
   dependencies = {
-    "frostplexx/mason-bridge.nvim",
+    "williamboman/mason.nvim",
   },
-  opts = {},
   config = function()
-    local timer = vim.uv.new_timer()
-    -- I don't now why but it works
-    timer:start(0, 0, vim.schedule_wrap(function()
-      require("lint").linters_by_ft = require("mason-bridge").get_linters()
-      require("lint").linters_by_ft.python = { "pylint" }
-      -- Why it doesn't work, how to debug ?
-      -- local pylint = require("lint").linters.pylint
-      -- pylint.args = { "--rcfile", "~/dev/mmsx/pylintrc" }
-      -- pylint.cmd = "/home/ilan/.cache/pypoetry/virtualenvs/mmsx-cjFlvnf_-py3.10/bin/pylint"
-    end))
-    -- require("lint").linters_by_ft = {
-    --   python = { "pylint" },
-    -- }
+    require("lint").linters_by_ft = {
+      json = { "jsonlint" },
+      python = { "pylint" },
+      bash = { "shellcheck" },
+      shell = { "shellcheck" },
+    }
+
+    if require("config/utils").is_intersec then
+      local pylint = require("lint").linters.pylint
+      pylint.cmd = "poetry"
+      pylint.args = vim.list_extend({ "run", "pylint" }, pylint.args)
+    end
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
