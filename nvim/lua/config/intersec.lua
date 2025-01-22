1c87415ed4f7998e9c0f9dc581ca24e325e843e9
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-01-22 15:45:14 +0100

    fix: daily warning

--- nvim/lua/config/intersec.lua
+++ nvim/lua/config/intersec.lua
@@ -2,7 +2,7 @@ local uv = vim.uv
 local timer = uv.new_timer()
 local s = 1000
 timer:start(0, 3 * s, vim.schedule_wrap(function()
-  if os.date("%H") == "10" and os.date("%M") == "42" then
+  if os.date("%H") == "10" and os.date("%M") == "58" then
     vim.notify_once("Daily", "info", {
       timeout = 240 * s,
     })
