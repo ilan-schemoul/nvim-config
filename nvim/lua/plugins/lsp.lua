26324014ef8e5787c33d7d33921588f4f104e809
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-01-22 15:44:58 +0100

    fix: pylint in intersec

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,74 +1,50 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    event = "LazyFile",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "RubixDev/mason-update-all",
-    },
-    config = function(_, _)
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- NOTE: At work we have virtual env with sometimes old node. Old node
-      -- don't work with LSP.
-      local path = "/home/ilan/.nvm/versions/node/v16.20.2/bin"
-      local cmd_env = { PATH = path .. ":" .. vim.env.PATH }
-
-      require'lspconfig'.fish_lsp.setup{}
-
-      require("mason-lspconfig").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            cmd_env = cmd_env,
-            capabilities = capabilities,
-          })
-        end,
-        ["clangd"] = function(_)
-          require("lspconfig").clangd.setup({
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--header-insertion=never"
-            }
-          })
-        end,
-        ["lua_ls"] = function(_)
-          require("lspconfig").lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" },
-                },
-              },
-            },
-          })
-        end,
-        ["pyright"] = function(_)
-          require("lspconfig").pyright.setup({
-            cmd_env = cmd_env,
-            capabilities = capabilities,
-            settings = {
-              python = {
-                analysis = {
-                  diagnosticMode = 'openFilesOnly',
-                },
-              },
-            },
-          })
-        end,
-        ["pylsp"] = function(_)
-          require("lspconfig").pyright.setup({
-            plugins = {
-              pylint = {
-                enabled = true,
-                executable = 'pylint',
-              },
-            }
-          })
-        end
-    })
-    end,
+return
+{
+  "neovim/nvim-lspconfig",
+  event = "LazyFile",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "RubixDev/mason-update-all",
   },
+  config = function(_, _)
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- NOTE: At work we have virtual env with sometimes old node. Old node
+    -- don't work with LSP.
+    local path = "/home/ilan/.nvm/versions/node/v16.20.2/bin"
+    local cmd_env = { PATH = path .. ":" .. vim.env.PATH }
+
+    require'lspconfig'.fish_lsp.setup{}
+
+    require("mason-lspconfig").setup()
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          cmd_env = cmd_env,
+          capabilities = capabilities,
+        })
+      end,
+      ["clangd"] = function(_)
+        require("lspconfig").clangd.setup({
+          capabilities = capabilities,
+          cmd = {
+            "clangd",
+            "--header-insertion=never"
+          }
+        })
+      end,
+      ["lua_ls"] = function(_)
+        require("lspconfig").lua_ls.setup({
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
+        })
+      end,
+    })
+  end,
 }
