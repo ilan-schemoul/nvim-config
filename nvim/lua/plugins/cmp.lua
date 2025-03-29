ee67ebcf7e1666d76c70c511312571f183c10c88
ilan schemoul <ilanschemoul@gmail.com>
2025-03-29 16:01:05 +0100

    feat: AI autocompletion
    Supermaven seems a bit better but can't use it at work. SO I'll have to
    use minuet AI + use something like vector code to improve it a bit.
    Later I'll add Codecompanion (with integration to VectorCode + MCPHub).
    I added MCPHub already to handle MCP servers which are servers that
    allow AIs to have more context (I think they just "ask questions" and
    local server answer ?)

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -64,7 +64,7 @@ return {
           end
         end),
 
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.locally_jumpable(1) then
@@ -74,7 +74,7 @@ return {
           end
         end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.locally_jumpable(-1) then
