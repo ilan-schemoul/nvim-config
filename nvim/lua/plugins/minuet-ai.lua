dea4835ccd378f89ccff8dc89e6dbc0857ebe968
ilan schemoul <ilanschemoul@gmail.com>
2025-03-30 20:53:42 +0200

    minuet

--- nvim/lua/plugins/minuet-ai.lua
+++ nvim/lua/plugins/minuet-ai.lua
@@ -3,7 +3,7 @@ local cortex = ''
 local url = require("config/utils").is_intersec() and cortex or cloud
 local provider = require("config/utils").is_intersec() and 'openai_fim_compatible' or 'codestral'
 
-return {
+local opts = {
   "milanglacier/minuet-ai.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -12,13 +12,13 @@ return {
   -- TODO: vector code
   opts = {
     debounce = 120,
-    throttle = 600,
+    throttle = 800,
     virtualtext = {
       auto_trigger_ft = { "*" },
       keymap = {
         -- TODO: stop monopilizing TAB like that
-        accept = '<A-d>',
-        accept_line = '<A-f>',
+        accept_line = '<A-d>',
+        accept = '<A-f>',
         -- accept n lines (prompts for number)
         -- e.g. "A-z 2 CR" will accept 2 lines
         accept_n_lines = '<A-z>',
@@ -29,19 +29,24 @@ return {
         dismiss = '<A-e>',
       },
       show_on_completion_menu = true,
-      provider = provider,
-      n_completions = 3,
-      context_window = 16000,
-      provider_options = {
-        openai_fim_compatible = {
-          end_point = url,
-          model = 'qwen2.5-coder:7b',
-          },
-          optional = {
-            max_tokens = 256,
-            stop = { '\n\n' },
-          },
-        },
-      },
+    },
+    n_completions = 3,
+    context_window = 16000,
+    provider = provider,
+  },
+}
+
+if require("config/utils").is_intersec() then
+  opts.opts.provider_options = {
+    openai_fim_compatible = {
+      end_point = url,
+      model = 'qwen2.5-coder:7b',
+    },
+    optional = {
+      max_tokens = 256,
+      stop = { '\n\n' },
     },
   }
+end
+
+return opts
