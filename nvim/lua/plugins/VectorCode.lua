c420876b3e9adffedda647fbbce20173f511bd6b
ilan schemoul <ilanschemoul@gmail.com>
2025-03-31 03:01:54 +0200

    minuet improve
    Add context based on Vector Code (very slow tho and not sure addin
    contexts works good with all AIs IDK), also require vectorization of
    entire codebase basically.
    Tried to bring changes history context to AI but doesn't seem to work
    great so far.

---
+++ nvim/lua/plugins/VectorCode.lua
@@ -0,0 +1,18 @@
+-- sudo apt install python3.11
+-- pipx install git+https://github.com/Davidyz/VectorCode --python python3.11
+-- I can also install vectorcode[lsp] and vectorcode[lsp,mcp]
+return {
+  enabled = os.getenv("NVIM_AI_ENABLE"),
+  "Davidyz/VectorCode",
+  cmd = "VectorCode",
+  version = "*", -- optional, depending on whether you're on nightly or release
+  build = "pipx upgrade vectorcode", -- optional but recommended if you set `version = "*"`
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    n_query = 3,
+    async_opts = {
+      n_query = 3,
+    }
+  },
+}
+
