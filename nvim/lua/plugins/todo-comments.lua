2cce79e35749a062ee3e03142b5345b91145df0d
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-04-01 15:46:12 +0200

    feat: go next todo

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,5 +1,10 @@
--- Underlines NOTE/HACK etc. in comments
 return {
-    "folke/todo-comments.nvim",
-    opts = { signs = false },
+  "folke/todo-comments.nvim",
+  cmd = { "TodoTrouble", "TodoTelescope" },
+  event = "LazyFile",
+  config = true,
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+  }
 }
