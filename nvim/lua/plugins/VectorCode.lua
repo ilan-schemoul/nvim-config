1adf208ae4df2587ed22f8283836066ec5d9d925
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-04-01 15:48:35 +0200

    fix

--- nvim/lua/plugins/VectorCode.lua
+++ nvim/lua/plugins/VectorCode.lua
@@ -2,7 +2,7 @@
 -- pipx install git+https://github.com/Davidyz/VectorCode --python python3.11
 -- I can also install vectorcode[lsp] and vectorcode[lsp,mcp]
 return {
-  enabled = os.getenv("NVIM_AI_ENABLE"),
+  enabled = os.getenv("NVIM_AI_ENABLE") ~= nil,
   "Davidyz/VectorCode",
   cmd = "VectorCode",
   version = "*", -- optional, depending on whether you're on nightly or release
@@ -15,4 +15,3 @@ return {
     }
   },
 }
-
