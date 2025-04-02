747986dabe99566bc7aa8b48e2236ded6f06ca2f
ilan schemoul <ilanschemoul@gmail.com>
2025-04-02 02:07:28 +0200

    refactor minuet

--- nvim/lua/plugins/minuet-ai.lua
+++ nvim/lua/plugins/minuet-ai.lua
@@ -4,6 +4,32 @@ local fim_endpoint = function(url)
   return 'https://' .. url .. '/v1/completions'
 end
 
+local cloud_qwen = {
+  api_key = 'NEBIUS_API_KEY',
+  end_point = fim_endpoint('api.studio.nebius.com'),
+  model = 'Qwen/Qwen2.5-Coder-7B',
+
+  name = 'Qwen',
+  optional = {
+    max_tokens = 56,
+    top_p = 0.9,
+  },
+}
+
+local cortex_qwen = {
+  api_key = 'TERM',
+  -- FIXME:
+  end_point = fim_endpoint('cortex.corp'),
+  -- FIXME:
+  model = 'qwen2.5-coder:7b',
+
+  name = 'Cortex qwen',
+  optional = {
+    max_tokens = 56,
+    top_p = 0.9,
+  },
+}
+
 return {
   enabled = os.getenv("NVIM_AI_ENABLE") ~= nil,
   "milanglacier/minuet-ai.nvim",
@@ -11,6 +37,8 @@ return {
     "nvim-lua/plenary.nvim",
   },
   opts = {
+    throttle = 800,
+    debounce = 250,
     virtualtext = {
       auto_trigger_ft = { "*" },
       keymap = {
@@ -29,25 +57,13 @@ return {
     },
     provider = is_intersec and 'openai_fim_compatible' or 'codestral',
     n_completions = is_intersec and 2 or 3,
-    context_window = is_intersec and 16000 or 4092,
+    context_window = is_intersec and 4092 or 14000,
     provider_options = {
       codestral = {
         end_point = 'https://api.mistral.ai/v1/fim/completions',
-        template = require("config/fim_prompts").codestral_template,
       },
 
-      openai_fim_compatible = {
-        -- NOTE: modify all at once
-        api_key = 'OPEN_ROUTER_API_KEY',
-        end_point = fim_endpoint('openrouter.ai/api'),
-        model = 'qwen/qwen-2.5-7b-instruct',
-
-        name = 'Ollama',
-        optional = {
-          max_tokens = 56,
-          top_p = 0.9,
-        },
-      },
+      openai_fim_compatible = is_intersec and cortex_qwen or cloud_qwen,
     },
   }
 }
