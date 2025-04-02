93636290dcba7e8e4644868d1f3270b5322be983
ilan schemoul <ilanschemoul@gmail.com>
2025-04-02 23:23:11 +0200

    disable neorg lazyload

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,8 +1,6 @@
 return {
   "nvim-neorg/neorg",
   version = "v9.x.x",
-  ft = "norg", -- lazy load on file type
-  cmd = "Neorg", -- lazy load on command
   config = function()
     require("neorg").setup({
       load = {
