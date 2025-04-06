9c5447543dcdd982015d9021b952a36cba7fb7a8
ilan schemoul <ilanschemoul@gmail.com>
2025-04-06 16:22:50 +0200

    feat: color for Markdown and codecompanion

---
+++ nvim/lua/plugins/markview.lua
@@ -0,0 +1,14 @@
+return {
+  "OXY2DEV/markview.nvim",
+  dependencies = {
+    "catppuccin/nvim",
+  },
+  ft = { "markdown", "codecompanion" },
+  opts = {
+    preview = {
+      filetypes = { "markdown", "codecompanion" },
+      ignore_buftypes = {},
+    }
+  }
+}
+
