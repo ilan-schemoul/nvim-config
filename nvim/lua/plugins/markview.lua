62ad3fa6065fc28f43c6bf1976e2709873d4b826
ilan schemoul <ilanschemoul@gmail.com>
2025-04-06 16:43:28 +0200

    improve md/codecompanion headers

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -8,7 +8,14 @@ return {
     preview = {
       filetypes = { "markdown", "codecompanion" },
       ignore_buftypes = {},
+    },
+  },
+  config = function(_, opts)
+    local presets = require("markview.presets");
+    opts.markdown = {
+      headings = presets.headings.simple,
     }
-  }
+    require("markview").setup(opts)
+  end,
 }
 
