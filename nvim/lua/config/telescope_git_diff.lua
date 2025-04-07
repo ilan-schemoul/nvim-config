57bbd09ae649585b0233ce2d332d001fede92ccc
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-04-07 16:30:08 +0200

    fix telescope git diff

--- nvim/lua/config/telescope_git_diff.lua
+++ nvim/lua/config/telescope_git_diff.lua
@@ -1,13 +1,20 @@
 local gitsigns = require("gitsigns")
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 
 local function git_modified_files()
   local modified_files = gitsigns.get_hunks() -- Fetch hunks (changes) tracked by Git
   local file_list = {}
 
+  if modified_files == nil then
+    return
+  end
+
   -- Collect modified file paths
   for _, file in pairs(modified_files) do
     for _, line in pairs(file.lines) do
-      table.insert(file_list, line)
+      local number = file.added.start
+      table.insert(file_list, { line = line, number = number })
     end
   end
 
@@ -17,9 +24,25 @@ local function git_modified_files()
     prompt_title = "Git Modified Files",
     finder = require("telescope.finders").new_table({
       results = file_list,
+      entry_maker = function(entry)
+        return {
+          display = entry.line,
+          ordinal = entry.line,
+          value = entry.line,
+          lnum = entry.number,
+        }
+      end,
     }),
     sorter = require("telescope.config").values.generic_sorter({}),
     previewer = require("telescope.previewers").vim_buffer_cat.new({}),
+    attach_mappings = function(prompt_bufnr, map)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local entry = action_state.get_selected_entry()
+        vim.cmd(tostring(entry.lnum))
+      end)
+      return true
+    end,
   })
   :find()
 end
