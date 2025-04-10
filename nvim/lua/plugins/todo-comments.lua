d471f3bbdf7f781e92d0b201094a85e14217cfaa
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-04-10 15:10:20 +0200

    fix: next todo

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -4,8 +4,8 @@ return {
   cmd = { "TodoTrouble", "TodoTelescope" },
   event = "LazyFile",
   keys = {
-    { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    { "]t", function() require("todo-comments").jump_next({keywords = { "TODO" }}) end, desc = "Next todo comment" },
+    { "[t", function() require("todo-comments").jump_prev({keywords = { "TODO" }}) end, desc = "Previous todo comment" },
   },
   opts = {
     search = {
