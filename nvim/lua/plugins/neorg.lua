60312e0fe1469ff97de3343f309b64abcd076f46
ilan schemoul <ilanschemoul@gmail.com>
2025-04-13 02:46:49 +0200

    perf: verylazy neorg
    For an extremely weird reason I cannot do filetype lazy loading.
    If I do filetype lazy loading AND I load the neorg file via Telescope
    AND Telescope settings has the layout_strategy vertical then most
    highlights do not work on Neorg (only some headings highlights work).

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-neorg/neorg",
   version = "v9.x.x",
+  event = "VeryLazy",
   config = function()
     require("neorg").setup({
       load = {
@@ -27,10 +28,13 @@ return {
         },
       },
     })
+
     vim.api.nvim_create_autocmd("Filetype", {
       pattern = "norg",
       callback = function()
-        vim.keymap.set("i", "<C-b>", "<Plug>(neorg.itero.next-iteration)", { buffer = true })
+        vim.keymap.set("i", "<C-b>", "<Plug>(neorg.itero.next-iteration)", {
+          buffer = true
+        })
       end,
     })
 
