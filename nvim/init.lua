ae3ccae9982c20cba638bb366275e61f06ecc267
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-05-02 19:18:56 +0200

    fix: path node
    Gives priority to nvim-internal-path (see fish commit to understand how
    I overwrite there path to asdf when non interactive)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
+-- I added this so I can have binaries that override the ones outside nvim
+vim.env.PATH = '/home/ilan/.bin/nvim-internal-path:' .. vim.env.PATH
+
 require("config/lazy")
 
 require("config/custom-commands")
