4c1405cab65add09f6b7ad5f5bca556c9804b80f
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-05-07 17:37:27 +0200

    refactor: use pylsp for pylint

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -6,16 +6,9 @@ return {
   config = function()
     require("lint").linters_by_ft = {
       json = { "jsonlint" },
-      python = { "pylint" },
       bash = { "shellcheck" },
       shell = { "shellcheck" },
     }
-
-    if require("config/utils").is_intersec then
-      local pylint = require("lint").linters.pylint
-      pylint.cmd = "poetry"
-      pylint.args = vim.list_extend({ "run", "pylint" }, pylint.args)
-    end
 
     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
       callback = function()
