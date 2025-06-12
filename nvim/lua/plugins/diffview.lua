35fc82d9c3cb7f289144cae1b075058256a96a95
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-06-12 10:56:00 +0200

    diffview 3 panels

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -3,6 +3,14 @@ return {
   cmd = {
     "DiffviewOpen",
   },
-  opts = {},
+  opts = {
+    view = {
+      merge_tool = {
+        layout = "diff4_mixed",
+        disable_diagnostics = true,   -- Temporarily disable diagnostics for diff buffers while in the view.
+        winbar_info = true,           -- See |diffview-config-view.x.winbar_info|
+      },
+    }
+  },
 }
 
