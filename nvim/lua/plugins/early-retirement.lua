eb1a513db6e526a3497d248908f3ef6c24b683e6
ilan schemoul <ilanschemoul@gmail.com>
2025-08-10 23:05:41 +0200

    retire buffers every 5 mn if at least 5

--- nvim/lua/plugins/early-retirement.lua
+++ nvim/lua/plugins/early-retirement.lua
@@ -3,7 +3,8 @@ local custom_commands = require("config/custom-commands")
 return {
     "chrisgrieser/nvim-early-retirement",
     opts = {
-        retirementAgeMins = 10,
+        minimumBufferNum = 5,
+        retirementAgeMins = 5,
         deleteFunction = custom_commands.close_buffer_if_not_last,
     },
     event = "VeryLazy",
