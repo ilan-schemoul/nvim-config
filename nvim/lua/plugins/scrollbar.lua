c3aa4534ccd860e4d38d34df5b5a945d0061234a
ilan schemoul <ilanschemoul@gmail.com>
2025-08-10 04:06:42 +0200

    doc

--- nvim/lua/plugins/scrollbar.lua
+++ nvim/lua/plugins/scrollbar.lua
@@ -3,7 +3,6 @@ return {
   enabled = false,
   event = "VimEnter",
   config = function()
-    require("gitsigns").setup()
     -- require("scrollbar.handlers.gitsigns").setup()
     require("scrollbar").setup()
   end,
