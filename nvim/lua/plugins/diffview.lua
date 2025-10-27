927ad06bba7e3df1398c1c0a57481e2d52dcc4bc
Ilan Schemoul <ilan.schemoul@intersec.com>
2025-10-27 14:43:17 +0100

    diffview, remove diff4

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -6,7 +6,6 @@ return {
   opts = {
     view = {
       merge_tool = {
-        layout = "diff4_mixed",
         disable_diagnostics = true,   -- Temporarily disable diagnostics for diff buffers while in the view.
         winbar_info = true,           -- See |diffview-config-view.x.winbar_info|
       },
