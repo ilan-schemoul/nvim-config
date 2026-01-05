9e95a087583e8833e845a2cc2cfa609bcb257bdb
Ilan Schemoul <ilan.schemoul@intersec.com>
2026-01-05 17:15:19 +0100

    update daily alert

--- nvim/lua/config/intersec.lua
+++ nvim/lua/config/intersec.lua
@@ -2,7 +2,8 @@ local uv = vim.uv
 local timer = uv.new_timer()
 local s = 1000
 timer:start(0, 3 * s, vim.schedule_wrap(function()
-  if os.date("%H") == "10" and os.date("%M") == "58" then
+  local min = os.date("%M")
+  if os.date("%H") == "9" and (min == "58" or min == "59") then
     vim.notify_once("Daily", "info", {
       timeout = 240 * s,
     })
