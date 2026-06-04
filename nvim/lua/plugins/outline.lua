f1c1eb55e4a3fba842be0a9497a4c82a89bb7145
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-06-04 18:53:02 +0200

    add outline because c#

---
+++ nvim/lua/plugins/outline.lua
@@ -0,0 +1,16 @@
+return {
+  "hedyhli/outline.nvim",
+  keys = {
+    { "<leader>so", "<cmd>Outline<cr>" },
+  },
+  opts = {
+    outline_window = {
+      position = "left",
+      auto_width = {
+        enabled = true,
+        max_width = 50,
+      },
+    },
+  },
+}
+
