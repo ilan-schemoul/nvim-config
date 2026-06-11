97e79ddcb3ceea0c5f432cd73b04177fbf54b160
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-06-11 22:52:04 +0200

    update notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -3,6 +3,8 @@ return {
   config = function()
     require("notify").setup({
       opts = 2,
+      background_colour = "#ffffff",
+      merge_duplicates = false
     })
 
     vim.notify = require("notify")
