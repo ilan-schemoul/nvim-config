745a2eac0cc199ce134ba4609217ab8550e60dc4
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-06-24 13:42:49 +0200

    go back upstream smart open

--- nvim/lua/plugins/smart-open.lua
+++ nvim/lua/plugins/smart-open.lua
@@ -1,9 +1,5 @@
 return {
-  -- "danielfalk/smart-open.nvim",
-  -- NOTE: https://github.com/danielfalk/smart-open.nvim/pull/80/files
-  -- He won't merge as is
-  "ilan-schemoul/smart-open.nvim",
-  -- dir = "~/code/forks/smart-open.nvim",
+  "danielfalk/smart-open.nvim",
   dependencies = {
     "kkharji/sqlite.lua",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -18,14 +14,8 @@ return {
     { "<leader>lL", "<cmd>Telescope smart_open<cr>" },
   },
   config = function(_, _)
-    local cfg = require('telescope._extensions.smart_open.default_config').ignore_patterns
-    local ignore_patterns_without_build = vim.tbl_filter(function(f)
-      return f ~= "*build/*"
-    end, cfg)
-
     local opts = {
       match_algorithm = "fzf",
-      ignore_patterns = ignore_patterns_without_build,
     }
     require("smart-open").setup(opts)
     require("telescope").load_extension("smart_open")
