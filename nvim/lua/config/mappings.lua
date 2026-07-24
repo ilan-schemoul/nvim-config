b14c3c6e1cd96c3d5cdcbf07e45f44891788e6ac
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-07-24 21:52:46 +0200

    refact

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -21,10 +21,10 @@ local function setv(keys, cmd)
 end
 
 local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  local count = next and 1 or -1
   severity = severity and vim.diagnostic.severity[severity] or nil
   return function()
-    go({ severity = severity })
+    vim.diagnostic.jump({ count = count, severity = severity })
   end
 end
 
