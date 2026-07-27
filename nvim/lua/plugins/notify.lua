b44ee9be0fc064511bc6e4fb8b19afa0e6ff8680
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-07-27 20:35:46 +0200

    perf: less fps notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -3,9 +3,10 @@ return {
   event = "BufEnter",
   config = function()
     require("notify").setup({
-      opts = 2,
+      fps = 10,
       background_colour = "#ffffff",
-      merge_duplicates = false
+      merge_duplicates = false,
+      stages = 'static'
     })
 
     vim.notify = require("notify")
