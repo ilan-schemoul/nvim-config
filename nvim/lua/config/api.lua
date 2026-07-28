a5d0d840228e5daa742068b9ce91507a4bccf7ae
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-07-28 14:15:05 +0200

    api expose function to toggle a fish terminal at a given cwd

--- nvim/lua/config/api.lua
+++ nvim/lua/config/api.lua
@@ -275,20 +275,21 @@ M.toggle_lazygit = function(force_new, cwd)
   M.toggle_or_create_sticky_term("lazygit", { "lazygit", "--path", root }, opts)()
 end
 
-local create_sticky_term = function(terminal_type, cmd, on_exit)
+local create_sticky_term = function(terminal_type, cmd, opts)
+  opts.env = opts.env or {}
+  opts.env.IS_FTERM = "1"
+
   ---@diagnostic disable-next-line: missing-fields
   sticky_terminals[terminal_type] = require('FTerm'):new({
-    ft = "ft_" .. terminal_type,
+    ft = opts.ft or ("ft_" .. terminal_type),
     cmd = cmd,
-    on_exit = on_exit,
+    on_exit = opts.on_exit,
     ---@diagnostic disable-next-line: missing-fields
-    dimensions = {
+    dimensions = opts.dimensions or {
         height = 0.95,
         width = 0.95
     },
-    env = {
-      IS_FTERM = "1"
-    },
+    env = opts.env,
   })
 end
 
@@ -298,7 +299,7 @@ local _toggle_or_create_sticky_term = function(terminal_type, cmd, opts)
   if sticky_terminals[terminal_type] ~= nil and not opts.force_new then
     buffer_terminal = sticky_terminals[terminal_type]:toggle()
   else
-    create_sticky_term(terminal_type, cmd, opts.on_exit)
+    create_sticky_term(terminal_type, cmd, opts)
     buffer_terminal = sticky_terminals[terminal_type]:open()
   end
 
@@ -333,6 +334,23 @@ M.force_new_sticky_term = function(terminal_type, cmd, opts)
   return M.toggle_or_create_sticky_term(terminal_type, cmd, opts)
 end
 
+-- One sticky fish terminal per cwd, cd'd into it via PRIO_HOME_DIR_STARTUP (see fish config.fish)
+M.toggle_or_create_fish_in_cwd = function(cwd)
+  -- used in ft, we cannot use paths (:/ etc.) so we hash it
+  local cwd_hash = cwd and vim.fn.sha256(cwd)
+  local terminal_type = "fish_" .. (cwd_hash or "default")
+  local env = { PRIO_HOME_DIR_STARTUP = cwd }
+
+  M.toggle_or_create_sticky_term(terminal_type, { "fish" }, {
+    env = env,
+    ft = 'ft_fish',
+    dimensions = {
+        height = 0.8,
+        width = 0.6
+    },
+  })()
+end
+
 M.kill_sticky_terminal = function(terminal_type)
   -- Not supported yet: https://github.com/numToStr/FTerm.nvim/issues/110
   sticky_terminals[terminal_type]:exit()
@@ -349,11 +367,7 @@ M.toggle_existing_sticky_term = function(terminal_type)
 end
 
 M.get_cwd = function()
-  local path = vim.fn.expand('%:h')
-
-  if not vim.fn.glob(path) then
-    path = vim.fn.getcwd()
-  end
+  local path = vim.fn.expand('%:p:h')
 
   -- NOTE: on terminal the path in its name is not updated when you cd, so it's
   -- rapidly out of date. It would be hard to get the current cwd from a running
@@ -361,9 +375,11 @@ M.get_cwd = function()
   if path:find("://") then
     local _, j = path:find("://")
     path = path:sub(j + 1, path:len())
+    local i, _ = path:find("//")
+    path = path:sub(1, i - 1)
   end
 
-  return path
+  return vim.fn.glob(path) or path
 end
 
 return M
