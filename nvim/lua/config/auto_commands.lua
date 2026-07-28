8ec2fea7415660be80b07967c8c5f4db8ef2bc81
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-07-28 23:05:33 +0200

    auto-rename terminal buffer based on current subprocess

--- nvim/lua/config/auto_commands.lua
+++ nvim/lua/config/auto_commands.lua
@@ -1,3 +1,4 @@
+local utils = require('config/utils')
 -- Absolute number if not focus
 vim.api.nvim_create_autocmd({ "WinEnter" }, {
   callback = function()
@@ -32,7 +33,7 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
 vim.api.nvim_create_autocmd("TermClose", {
   pattern = "*",
   callback = function()
-    -- XXX: stop closing on error (e.g.: lazygit crash)
+    -- XXX: disable for all fterm and handle it myself
     if (vim.bo.buftype == "terminal" or vim.bo.filetype == "lua") and vim.v.shell_error == 0 then
       -- Let me see errors
       if vim.bo.ft == "ft_aspire" or vim.bo.ft == "ft_lazygit" then
@@ -50,3 +51,29 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.cmd("setfiletype img")
   end,
 })
+
+local create_term_name = function(process)
+  local bin = vim.fn.exepath(process.name)
+
+  return "term://" .. vim.fn.getcwd() .. "//" .. process.pid .. ":" .. bin
+end
+
+local original_term_name = {}
+
+-- Can be a bit slow 50/100ms
+vim.api.nvim_create_autocmd({ "TermLeave"  }, {
+  callback = function()
+    local channel = vim.bo[0].channel
+    local child_process = vim.api.nvim_get_proc_children(vim.fn.jobpid(channel))
+    if #child_process > 0 then
+      local process = vim.api.nvim_get_proc(child_process[1])
+      local new_name = create_term_name(process)
+
+      original_term_name[vim.bo[0].channel] = vim.api.nvim_buf_get_name(0)
+      vim.api.nvim_buf_set_name(0, new_name)
+    elseif original_term_name[vim.bo[0].channel] then
+      vim.api.nvim_buf_set_name(0, original_term_name[vim.bo[0].channel])
+    end
+  end,
+})
+
