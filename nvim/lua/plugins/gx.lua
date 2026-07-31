df6d4f61dcc5808276ac71ad0d3398b1d81f554c
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-07-31 15:12:15 +0200

    open jira link gx

--- nvim/lua/plugins/gx.lua
+++ nvim/lua/plugins/gx.lua
@@ -3,6 +3,21 @@ return {
   -- Search on the internet
   keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
   cmd = { "Browse" },
+  opts = {
+    select_prompt = false,
+    handlers = {
+      jira = {
+        name = "jira", -- set name of handler
+        handle = function(mode, line, _)
+          local ticket_nb = require("gx.helper").find(line, mode, "MG%-(%d+)")
+
+          if ticket_nb and #ticket_nb <= 5 then
+            return "https://metalgear.atlassian.net/browse/MG-" .. ticket_nb
+          end
+        end,
+      },
+    },
+  },
   init = function()
     vim.g.netrw_nogx = 1 -- disable netrw gx
   end,
