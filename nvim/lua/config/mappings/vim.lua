729ea13a6b9913339a2018e941a9ebcb8d466975
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-02 00:50:38 +0200

    resolve nvim config path to avoid ".."

--- nvim/lua/config/mappings/vim.lua
+++ nvim/lua/config/mappings/vim.lua
@@ -3,7 +3,8 @@ local api = require('config/api')
 local set = api.keymap.leader
 
 local nvim_config_path = vim.uv.fs_realpath(vim.fn.expand("~/.config/nvim"))
-local nvim_root_path = vim.fn.expand(nvim_config_path .. "/..")
+local nvim_root_path = vim.fn.fnamemodify(nvim_config_path, ":h")
+vim.print(nvim_root_path)
 nvim_config_path = nvim_config_path or "~/.config/nvim"
 
 set("vpl", "<cmd>Telescope find_files search_dirs=~/.local/share/nvim/lazy<cr>", "Find files in installed plugins")
