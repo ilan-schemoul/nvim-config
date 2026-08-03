1206756b7d260418ff6dbc4a8c44a1a27d0f43ef
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-03 21:16:01 +0200

    perf: remove internal unused path

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,3 @@
--- I added this so I can have binaries that override the ones outside nvim
-vim.env.PATH = '/home/ilan/.bin/nvim-internal-path:' .. vim.env.PATH
-
 require("config/lazy")
 
 require("config/native_plugins")
