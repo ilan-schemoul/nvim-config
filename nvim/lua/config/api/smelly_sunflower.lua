979a2acc5d3548fe1bb8732d0646f2f32802f174
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-03 22:59:12 +0200

    improve smelly_sunflower

--- nvim/lua/config/api/smelly_sunflower.lua
+++ nvim/lua/config/api/smelly_sunflower.lua
@@ -3,66 +3,80 @@ local M = {}
 
 math.randomseed(vim.uv.hrtime())
 
-local function random_adjective()
-    local words = {
-    "beautiful", "witty", "wicked", "confusing", "rich", "new", "strange",
-    "rocky", "circular", "helpful", "competent", "smelly", "stable", "grumpy",
-    "devoted", "smart", "muscular", "graceful", "scary", "safe", "wooden", "sleepy",
-    "tardy", "hungry", "strange", "hopeful", "proud", "new", "dainty", "royal",
-    "arrogant", "round", "efficient", "youthful", "cumbersome", "fickle",
-    "mild", "expensive", "small", "rude", "generous", "courageous",
-    "zany", "thin", "round", "oval", "dark", "hot", "modern", "petite",
-    "weary",
-    }
-    return words[math.random(1, #words)]
-end
+local adjectives = {
+    "epice", "maudit", "feroce", "dodu", "louche", "bancal", "loufoque", "piquant", "grincheux", "sournois", "chaotique", "branlant",
+    "fourbe", "bougon", "revolte", "diabolique", "capricieux", "desordonne", "espiegle", "malicieux",
+}
+
+local emojis = {
+    raton = "🦝", clown = "🤡", fantome = "👻", licorne = "🦄", poulpe = "🐙", cactus = "🌵", cerveau = "🧠", monstre = "🍝", chat = "🐈",
+    bombe = "💣", taco = "🌮", zombie = "🧟", extraterrestre = "👽", diable = "😈", sirene = "🧜", robot = "🤖", pingouin = "🐧",
+    banane = "🍌", tatou = "🦔", dragon = "🐉", hippopotame = "🦛", crocodile = "🐊", serpent = "🐍", scorpion = "🦂", araignee = "🕷️",
+    abeille = "🐝", papillon = "🦋", escargot = "🐌", tortue = "🐢", lezard = "🦎", requin = "🦈", baleine = "🐳", dauphin = "🐬",
+    poisson = "🐟", renard = "🦊", loup = "🐺", ours = "🐻", panda = "🐼", koala = "🐨", tigre = "🐯", lion = "🦁", singe = "🐵",
+    cochon = "🐷", mouton = "🐑", vache = "🐮", poulet = "🐔", hibou = "🦉", chauvesouris = "🦇", souris = "🐭", hamster = "🐹",
+}
 
 local function random_emojis()
-  local emojis = {
-    cat='🐱',  dog='🐶',  mouse='🐭',  hamster='🐹',  rabbit='🐰',  wolf='🐺',  frog='🐸',  tiger='🐯',  koala='🐨',  bear='🐻',  pig='🐷',  pig_nose='🐽',  cow='🐮',  boar='🐗',  monkey_face='🐵',  monkey='🐒',  horse='🐴',  racehorse='🐎',  camel='🐫',  sheep='🐑',  elephant='🐘',  panda_face='🐼',  snake='🐍',  bird='🐦',  baby_chick='🐤',  hatched_chick='🐥',  hatching_chick='🐣',  chicken='🐔',  penguin='🐧',  turtle='🐢',  bug='🐛',  honeybee='🐝',  ant='🐜',  beetle='🪲',  snail='🐌',  octopus='🐙',  tropical_fish='🐠',  fish='🐟',  whale='🐳',  dolphin='🐬',  ram='🐏',  rat='🐀',  water_buffalo='🐃',  dragon='🐉',  goat='🐐',  rooster='🐓',  ox='🐂',  dragon_face='🐲',  blowfish='🐡',  crocodile='🐊',  dromedary_camel='🐪',  leopard='🐆',  poodle='🐩',  paw_prints='🐾',  bouquet='💐',  cherry_blossom='🌸',  tulip='🌷',  four_leaf_clover='🍀',  rose='🌹',  sunflower='🌻',  hibiscus='🌺',  maple_leaf='🍁',  leaves='🍃',  fallen_leaf='🍂',  herb='🌿',  mushroom='🍄',  cactus='🌵',  palm_tree='🌴',  deciduous_tree='🌳',  chestnut='🌰',  seedling='🌱',  blossom='🌼',
-  }
+    local keyset={}
+    local n=0
+    for k in pairs(emojis) do
+        n=n+1
+        keyset[n]=k
+    end
 
-  local keyset={}
-  local n=0
-  for k,v in pairs(emojis) do
-    n=n+1
-    keyset[n]=k
-  end
+    local desc = keyset[math.random(1, n)]
+    return desc, emojis[desc]
+end
 
-  local key = keyset[math.random(1, n)]
-  return key, emojis[key]
+local used_combos = {}
+
+local function random_combo()
+    local adjective = adjectives[math.random(1, #adjectives)]
+    local emoji_desc, emoji = random_emojis()
+    local combo_key = adjective .. '|' .. emoji_desc
+
+    local total = vim.tbl_count(emojis) + vim.tbl_count(adjectives)
+    assert(vim.tbl_count(used_combos) < total, "All emoji/adjectives combo used")
+
+    if used_combos[combo_key] then
+        return random_combo()
+    end
+
+    used_combos[combo_key] = true
+    return adjective, emoji_desc, emoji
 end
 
 local function insert_log(above)
-    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local row = vim.api.nvim_win_get_cursor(0)[1]
     if above then
-      row = row - 1
+        row = row - 1
     end
-    local word, emoji = random_emojis()
-    local payload =  emoji .. ' ' .. random_adjective() .. ' ' .. word .. ' fix_me_now'
+    local adjective, emoji_desc, emoji = random_combo()
+    local payload =  emoji .. ' ' .. emoji_desc .. ' ' .. adjective .. ' fix_me_now'
 
     local log
     local ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
     if ft == "c" then
-      log = 'logger_warning(&_G.logger, "' .. payload .. ' %s:%d' .. '", __FILE__, __LINE__);'
+        log = 'logger_warning(&_G.logger, "' .. payload .. ' %s:%d' .. '", __FILE__, __LINE__);'
     elseif ft == "python" then
-      log = 'LOGGER.warning("' .. payload .. '")'
+        log = 'LOGGER.warning("' .. payload .. '")'
     elseif ft == "cs" then
-      log = 'System.Console.WriteLine("' .. payload .. '");'
+        log = 'System.Console.WriteLine("' .. payload .. '");'
     else
-      vim.notify("No debug log template for filetype: " .. ft, vim.log.levels.ERROR)
-      return
+        vim.notify("No debug log template for filetype: " .. ft, vim.log.levels.ERROR)
+        return
     end
     vim.api.nvim_buf_set_lines(0, row, row, false, { log })
 
     if above then
-      vim.fn.feedkeys("k")
+        vim.fn.feedkeys("k")
     else
-      vim.fn.feedkeys("j")
+        vim.fn.feedkeys("j")
     end
 
     vim.schedule(function()
-      vim.fn.feedkeys("==")
+        vim.fn.feedkeys("==")
     end)
 end
 
@@ -70,44 +84,44 @@ function M.insert_log_above() insert_log(true) end
 function M.insert_log_below() insert_log(false) end
 
 function M.clean_logs()
-  vim.cmd("g/fix_me_now/d")
+    vim.cmd("g/fix_me_now/d")
 end
 
 function M.clean_all_logs()
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    local readonly = vim.api.nvim_get_option_value("readonly", { buf = bufnr })
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        local readonly = vim.api.nvim_get_option_value("readonly", { buf = bufnr })
 
-    if (vim.api.nvim_buf_is_valid(bufnr)
-        and vim.api.nvim_get_option_value("modifiable", { buf = bufnr })
-        and not readonly
-        and vim.api.nvim_get_option_value("buftype", { buf = bufnr }) == '') then
-      local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-      local deleted_lines = 0
-      for i, line in ipairs(lines) do
-        if string.match(line, "fix_me_now") then
-          local line_to_delete = i - deleted_lines
-          vim.api.nvim_buf_set_lines(bufnr, line_to_delete - 1, line_to_delete, false, {})
-          deleted_lines = deleted_lines + 1
+        if (vim.api.nvim_buf_is_valid(bufnr)
+            and vim.api.nvim_get_option_value("modifiable", { buf = bufnr })
+            and not readonly
+            and vim.api.nvim_get_option_value("buftype", { buf = bufnr }) == '') then
+            local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+            local deleted_lines = 0
+            for i, line in ipairs(lines) do
+                if string.match(line, "fix_me_now") then
+                    local line_to_delete = i - deleted_lines
+                    vim.api.nvim_buf_set_lines(bufnr, line_to_delete - 1, line_to_delete, false, {})
+                    deleted_lines = deleted_lines + 1
+                end
+            end
+
+            if vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)[1] ~= nil and vim.api.nvim_buf_get_name(bufnr) ~= '' then
+
+                vim.api.nvim_buf_call(bufnr, function() vim.cmd('w') end)
+            end
         end
-      end
-
-      if vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)[1] ~= nil and vim.api.nvim_buf_get_name(bufnr) ~= '' then
-
-        vim.api.nvim_buf_call(bufnr, function() vim.cmd('w') end)
-      end
     end
-  end
 end
 
 -- Wrap a function so calling it notifies how long it took
 function M.profile(fn)
-  return function(...)
-    local start = vim.uv.hrtime()
-    local ret = fn(...)
-    vim.notify(("It took %.3fms"):format((vim.uv.hrtime() - start) / 1e6))
+    return function(...)
+        local start = vim.uv.hrtime()
+        local ret = fn(...)
+        vim.notify(("It took %.3fms"):format((vim.uv.hrtime() - start) / 1e6))
 
-    return ret
-  end
+        return ret
+    end
 end
 
 return M
