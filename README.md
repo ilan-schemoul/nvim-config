b5a0a1baa3bd2bc9a18aa42c7216813be1614bcd
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-09 03:09:09 +0200

    improve naming telescope title

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ gv last selection
 - Improve lnav (go to exception)
 - Improve ast grep queries for wolverine
 - use claude servers (like the one to include code and one for logs)
+- gitsigns stopped working if outside cwd ? (or never worked ?)
 
 ## High priority
 - Use sfln (from vs code if exist, otherwise create)
--- nvim/lua/config/api/telescope.lua
+++ nvim/lua/config/api/telescope.lua
@@ -1,27 +1,37 @@
 local M = {}
 
-local file = require('config/api/file')
+local function telescope_args()
+  local oil_dir = require("oil").get_current_dir()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  local git_root = vim.fs.root(bufname, ".git")
+  local root = oil_dir or git_root
+  local title = vim.fn.fnamemodify(root, ":~")
+  vim.print(title)
+
+  return {
+    cwd = root,
+    prompt_title = title,
+  }
+end
 
 -- Live grep from the Oil directory when called from an Oil buffer, cwd otherwise
 function M.live_grep()
-  local dir = file.cwd()
-  local title = vim.fn.fnamemodify(dir, ":~")
+  local opts = telescope_args()
 
   require("telescope.builtin").live_grep({
-    cwd = dir,
-    prompt_title = title,
+    cwd = opts.cwd,
+    prompt_title = " Grep in " .. opts.prompt_title,
   })
 end
 
 function M.smart_open()
-  local dir = file.cwd()
-  local title = vim.fn.fnamemodify(dir, ":~")
+  local opts = telescope_args()
 
-  require('telescope').extensions.smart_open.smart_open {
-    cwd = dir,
-    prompt_title = title,
+  require('telescope').extensions.smart_open.smart_open({
+    cwd = opts.cwd,
     cwd_only = true,
-  }
+    prompt_title = "󰱼 Find a file in " .. opts.prompt_title,
+  })
 end
 
 return M
