3af76adc4dc2148f9556c4ecd26e243f7441c291
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-12 11:54:05 +0200

    remove dead code

--- nvim/lua/plugins/oil-git-status.lua
+++ nvim/lua/plugins/oil-git-status.lua
@@ -4,19 +4,6 @@ return {
   dependencies = {
     "stevearc/oil.nvim",
   },
-  config = function()
-    require('oil-git-status').setup()
-
-    vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "*",
-      callback = function()
-        if vim.bo.filetype == "oil" then
-          vim.cmd('set statuscolumn=')
-        elseif vim.bo.buftype == '' then
-          vim.cmd('set statuscolumn=%s')
-        end
-      end,
-    })
-  end,
+  opts = {}
 }
 
