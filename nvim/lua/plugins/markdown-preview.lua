fa58285640335910ca69f5e9bbfc07478b3d2a89
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-17 09:53:59 +0200

    stop auto close markdown preview

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,9 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  init = function()
+    vim.g.mkdp_auto_close = 0
+  end,
   ft = { "markdown" },
   build = ":call mkdp#util#install()",
 }
