be0f13b08e2a2855533d39ca8db9794fd23dcc01
Ilan Schemoul <ilan.schemoul@gmail.com>
2026-08-22 11:40:14 +0200

    c-n/c-p to jump next/previous buffer
    Since I lazy loaded the plugin c-n/c-p didnt work unless I did a-o/a-i first

--- nvim/lua/plugins/bufjump.lua
+++ nvim/lua/plugins/bufjump.lua
@@ -2,11 +2,17 @@ return {
   "kwkarlwang/bufjump.nvim",
   keys = {
     "<A-o>",
-    "<A-i>"
+    "<A-i>",
+    "<c-n>",
+    "<c-p>",
   },
   opts = {
+    -- Jump inside one buffer only
     backward_same_buf_key = "<A-o>",
     forward_same_buf_key = "<A-i>",
+    -- Jump from one buffer to next
+    forward_key = "<C-n>",
+    backward_key = "<C-p>",
   },
 }
 
